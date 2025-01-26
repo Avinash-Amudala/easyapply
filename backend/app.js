c625c9d879ca4c4e833228043d7d27e1c9bd2372
ajaits <ajaits@rit.edu>
2025-01-26 11:57:57 -0800

    fixed backend issues

--- backend/app.js
+++ backend/app.js
@@ -35,6 +35,7 @@ mongoose
     .connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 5000,
     })
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('MongoDB connection error:', err));
