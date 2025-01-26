e2c2795f03ce4730bfc871710612e56d5fa51f01
ajaits <ajaits@rit.edu>
2025-01-26 12:35:41 -0800

    fixed backend issues

--- backend/app.js
+++ backend/app.js
@@ -14,18 +14,16 @@ const allowedOrigins = [
     'http://localhost:3000'
 ];
 
-app.use(
-    cors({
-        origin: function (origin, callback) {
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(new Error('Not allowed by CORS'));
-            }
-        },
-        credentials: true,
-    })
-);
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+}));
 
 app.use(express.json());
 
